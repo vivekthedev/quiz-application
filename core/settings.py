e851e9448e22347a7df7c25e5f881106d02247b1
Vivek <contactvivekhere@gmail.com>
2023-01-05 23:13:13 +0530

    user model set up

--- core/settings.py
+++ core/settings.py
@@ -126,3 +126,4 @@ STATICFILES_DIR = [
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
+AUTH_USER_MODEL = "quiz.User"

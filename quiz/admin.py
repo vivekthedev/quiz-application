2fa2f71c7356daaacdabc16c7370985a4f02266a
Vivek <contactvivekhere@gmail.com>
2023-01-07 13:04:22 +0530

    static setup

--- quiz/admin.py
+++ quiz/admin.py
@@ -1,3 +1,61 @@
 from django.contrib import admin
+from .models import User, Quiz, Question, Option, Subject
 
-# Register your models here.
+
+class BaseTabularInline(admin.TabularInline):
+    show_change_link = True
+    extra = 3
+
+
+class QuizInline(BaseTabularInline):
+    model = Quiz
+
+
+class QuestionInline(BaseTabularInline):
+    model = Question
+
+
+class OptionInline(BaseTabularInline):
+    model = Option
+    max_num = 3
+
+
+@admin.register(User)
+class UserAdmin(admin.ModelAdmin):
+    list_display = ["get_full_name", "username", "email"]
+    search_fields = ("first_name", "last_name", "email", "username")
+
+
+@admin.register(Subject)
+class SubjectAdmin(admin.ModelAdmin):
+    list_display = ["name", "sub_code"]
+    search_fields = ("name",)
+    inlines = [
+        QuizInline,
+    ]
+
+
+@admin.register(Quiz)
+class QuizAdmin(admin.ModelAdmin):
+    list_display = ["id", "subject", "authored_by"]
+    search_fields = ("subject",)
+    inlines = [
+        QuestionInline,
+    ]
+
+
+@admin.register(Question)
+class QuestionAdmin(admin.ModelAdmin):
+    list_display = [
+        "name",
+    ]
+    search_fields = ("name",)
+    inlines = [
+        OptionInline,
+    ]
+
+
+@admin.register(Option)
+class OptionAdmin(admin.ModelAdmin):
+    list_display = ["name", "is_correct"]
+    search_fields = ("name",)
--- quiz/models.py
+++ quiz/models.py
@@ -1,6 +1,65 @@
 from django.db import models
 from django.contrib.auth.models import AbstractUser
+import uuid
 
 
 class User(AbstractUser):
     pass
+
+    class Meta:
+        verbose_name = "User"
+        verbose_name_plural = "Users"
+
+
+class Subject(models.Model):
+    name = models.CharField(max_length=200)
+    sub_code = models.CharField(max_length=200)
+
+    def __str__(self):
+        return self.name
+
+    class Meta:
+        ordering = ("name",)
+        verbose_name = "Subject"
+        verbose_name_plural = "Subjects"
+
+
+class Quiz(models.Model):
+    id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
+    subject = models.ForeignKey(Subject, on_delete=models.CASCADE)
+    authored_on = models.DateTimeField(auto_now=True)
+    authored_by = models.ForeignKey(User, on_delete=models.CASCADE)
+    is_private = models.BooleanField(default=False)
+
+    def __str__(self):
+        return str(self.id)
+
+    class Meta:
+        ordering = ("-authored_on",)
+        verbose_name = "Quiz"
+        verbose_name_plural = "Quizzes"
+
+
+class Question(models.Model):
+    name = models.CharField(max_length=800)
+    quiz = models.ForeignKey(Quiz, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return self.name
+
+    class Meta:
+        verbose_name = "Question"
+        verbose_name_plural = "Questions"
+
+
+class Option(models.Model):
+    name = models.CharField(max_length=200)
+    is_correct = models.BooleanField(default=False)
+    question = models.ForeignKey(Question, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return self.name
+
+    class Meta:
+        verbose_name = "Option"
+        verbose_name_plural = "Options"
